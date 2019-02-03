4c335dd8b2b9de911adfe61ee57c66045245bb6d
Ahmet Karalar <akaralar@gmail.com>
2019-02-04 02:42:53 +0300

    Add .gitignore and initiall commit

---
+++ Mediate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mediate
+//
+//  Created by Ahmet Karalar on 4.02.2019.
+//  Copyright © 2019 Ahmet Karalar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
